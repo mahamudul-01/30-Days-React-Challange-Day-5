ffaf5b38a8c6b8a1a7efaf174e7b8f021c7f5b36
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-01-13 21:47:07 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
+import profile from '../../assets/images/profile.png'
 
+// Header component that displays the title and profile image
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center px-4 py-2 border-b-2'>
             <h1 className="text-4xl font-bold">
+                {/* Display the title */}
                 Knowledge Cafe
             </h1>
+            {/* Display the profile image */}
+            <img src={profile} alt="Profile" />
         </div>
     );
 };
